5eae9c3e3f3e960254861611fd87c52f5471a3ee
Lucas Bertrand-Christen <l.b-c@hotmail.com>
2023-05-10 17:26:14 +0200

    create-map

--- src/main/java/fr/ul/miage/lutakhato/Server.java
+++ src/main/java/fr/ul/miage/lutakhato/Server.java
@@ -1,7 +1,12 @@
 package fr.ul.miage.lutakhato;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Server {
+
+    Map<String, Object> database = new HashMap<String, Object>();
     public static void main(String[] args){
-        
+
     }
 }
