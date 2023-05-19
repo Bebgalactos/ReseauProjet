7443602fc1972b0bb5c880f60eac48fe203a35ea
khawla <khawlaghninou16@gmail.com>
2023-05-19 14:55:49 +0200

    test exist

---
+++ src/test/java/fr/ul/miage/lutakhato/ServerExistTest.java
@@ -0,0 +1,62 @@
+package fr.ul.miage.lutakhato;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ServerExistTest {
+
+    private Server server;
+
+    @BeforeEach
+    public void setUp() {
+        server = new Server();
+        server.getDatabase().put("key1", new ServerObject(0, "valeur1"));
+        server.getDatabase().put("key2", new ServerObject(0, "valeur2"));
+        server.getDatabase().put("key3", new ServerObject(0, "valeur3"));
+    }
+
+    @Test
+    public void testExistsAvecClesExistantes() {
+        // Appeler la méthode exists avec des clés existantes
+        int nbSuccess = server.exists(new String[]{"key1", "key2"});
+
+        // Vérifier que le nombre de clés existantes est correct
+        Assertions.assertEquals(2, nbSuccess);
+    }
+
+    @Test
+    public void testExistsAvecClesExistantesEtNonExistantes() {
+        // Appeler la méthode exists avec des clés existantes et non existantes
+        int nbSuccess = server.exists(new String[]{"key1", "key2", "key4", "key5"});
+
+        // Vérifier que le nombre de clés existantes est correct
+        Assertions.assertEquals(2, nbSuccess);
+    }
+
+    @Test
+    public void testExistsAvecClesSansExpiration() {
+        // Ajouter des clés sans expiration dans la base de données
+        server.getDatabase().put("key1", new ServerObject(-1, "valeur1"));
+        server.getDatabase().put("key2", new ServerObject(-1, "valeur2"));
+
+        // Appeler la méthode exists avec les clés sans expiration
+        int nbSuccess = server.exists(new String[]{"key1", "key2"});
+
+        // Vérifier que le nombre de clés existantes est correct
+        Assertions.assertEquals(2, nbSuccess);
+    }
+
+
+
+
+
+}
+
