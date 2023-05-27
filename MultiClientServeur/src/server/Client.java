044802297ce4a8aaaa3e2092dd104240c343c35d
khawlaa1622 <131205737+khawlaa1622@users.noreply.github.com>
2023-05-27 18:05:45 +0200

    Add files via upload

---
+++ MultiClientServeur/src/server/Client.java
@@ -0,0 +1,68 @@
+package server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private InputStream in;
+    private OutputStream out;
+
+    public Client(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = socket.getInputStream();
+        this.out = socket.getOutputStream();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Socket clientSocket = new Socket("localhost", 1337);
+        Client client = new Client(clientSocket);
+        client.interact();
+    }
+
+    public void interact() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("You> ");
+            final String input = scanner.nextLine();
+            this.out.write(input.getBytes(StandardCharsets.UTF_8));
+            this.out.flush();
+
+            byte[] buffer = new byte[20];
+            int numRead;
+
+            StringBuilder responseBuilder = new StringBuilder();
+            while ((numRead = this.in.read(buffer)) != -1) {
+                responseBuilder.append(new String(buffer, 0, numRead, StandardCharsets.UTF_8));
+                if (numRead < buffer.length) {
+                    break;
+                }
+            }
+
+            String response = responseBuilder.toString();
+            System.out.println("Server> " + response);
+
+            if (response.equals("exit")) {
+                System.out.println("Déconnexion du serveur.");
+                break;
+            }
+        }
+    }
+
+    public void close() throws IOException {
+        this.socket.close();
+    }
+
+    public InputStream getInputStream() {
+        return this.in;
+    }
+
+    public OutputStream getOutputStream() {
+        return this.out;
+    }
+}
---
+++ MultiClientServeur/src/user/Publisher.java
@@ -0,0 +1,30 @@
+package user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Publisher {
+
+    private Map<Channel, String> channels;
+    public Publisher() {
+        this.channels = new HashMap<>();
+    }
+
+    public Map<Channel, String> getChannels() {
+        return channels;
+    }
+
+    public void setChannels(Map<Channel, String> channels) {
+        this.channels = channels;
+    }
+
+    public void createChannel(String name) {
+        channels.put(new Channel(name),name);
+    }
+
+    public void deleteChannel(String name) {
+        channels.remove(name);
+    }
+
+}
+
